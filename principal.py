57f44d5dfbce84e3ae1f7b91d064e7b78ba2c803
SilvinaRomero <sromero.fernandez@gmail.com>
2023-01-15 21:15:28 +0100

    agrego una nueva función

--- principal.py
+++ principal.py
@@ -7,6 +7,11 @@ from bson import ObjectId
 from pymongo import MongoClient
 import os
 
+def comprueba_fondo():
+    pass
+
+DOCUMENTOS = ["doc","docx"]
+
 # creamos app flask y lanzamos el servidor
 app = Flask(__name__)
 
