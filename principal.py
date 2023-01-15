950d235276406b0d83250203860c40393e6693ae
SilvinaRomero <111603715+SilvinaRomero@users.noreply.github.com>
2023-01-15 21:40:07 +0100

    Update principal.py

--- principal.py
+++ principal.py
@@ -36,8 +36,8 @@ app.config["UPLOAD_FOLDER"] = './static/fondos'
 # configuración para hacerlo desde casa
 app.config["MAIL_SERVER"] = "mail.gmx.es"
 app.config["MAIL_PORT"] = "587"
-app.config["MAIL_USERNAME"] = "silvina.romero@gmx.com"
-app.config["MAIL_PASSWORD"] = "SCaner80o"
+app.config["MAIL_USERNAME"] = ""
+app.config["MAIL_PASSWORD"] = ""
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
 mail = Mail(app)
